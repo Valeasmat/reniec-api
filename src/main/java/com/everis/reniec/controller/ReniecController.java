87ea8bee4909386dc93c39ea238a019618dbc42c
Valeasmat <valeriaasmat2dob@gmail.com>
2021-01-06 14:08:02 -0500

    swagger config

--- src/main/java/com/everis/reniec/controller/ReniecController.java
+++ src/main/java/com/everis/reniec/controller/ReniecController.java
@@ -3,6 +3,8 @@ package com.everis.reniec.controller;
 import com.everis.reniec.dto.ATMDepositRequest;
 import com.everis.reniec.dto.ReniecResponse;
 import io.reactivex.Single;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReniecController {
-
+    @ApiOperation(value = "Post request to retrieve reniec validation",
+            response = ReniecResponse.class,
+            produces = "application/stream+json")
+    @ApiResponse(code = 200,message = "Successful operation",response = ReniecResponse.class)
     @PostMapping(value = "/external/reniec/validate",produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Single<ReniecResponse> validate(@RequestBody ATMDepositRequest request){
         return Single.just(new ReniecResponse("Reniec",true));
--- src/main/java/com/everis/reniec/dto/ReniecResponse.java
+++ src/main/java/com/everis/reniec/dto/ReniecResponse.java
@@ -1,6 +1,8 @@
 package com.everis.reniec.dto;
 
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 @Getter
@@ -8,8 +10,10 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@ApiModel("Reniec response")
 public class ReniecResponse {
-
+    @ApiModelProperty(example = "Reniec")
     private String entityName;
+    @ApiModelProperty(example = "true")
     private Boolean success;
 }
